06c10fa23a32dd2f1c30bfc26ecf1de139dc01e9
Cəfər Mustafayev <114340748+JafarMustafayev@users.noreply.github.com>
2024-11-28 17:31:17 +0400

    Refactor Message component to clean up code and improve modal image display

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 const Message = ({ message, isUser, isLoading, image }) => {
   const [modalImage, setModalImage] = useState(null);
 
-  // Mesaj formatlama funksiyası
   const formatMessage = (text) => {
     return text.split(/(\*\*.*?\*\*)/g).map((part, index) => {
       const isBold = part.startsWith("**") && part.endsWith("**");
@@ -19,12 +18,10 @@ const Message = ({ message, isUser, isLoading, image }) => {
     });
   };
 
-  // Modal bağlama funksiyası
   const closeModal = () => setModalImage(null);
 
   return (
     <>
-      {/* Mesaj komponenti */}
       <div className={`flex ${isUser ? "justify-end" : "justify-start"}`}>
         <div className="text-white max-w-[85%] md:max-w-[70%]">
           {/* Şəkil varsa göstər */}
@@ -46,7 +43,6 @@ const Message = ({ message, isUser, isLoading, image }) => {
             </div>
           )}
 
-          {/* Mesajın məzmunu */}
           <div className={`flex ${isUser ? "justify-end" : "justify-start"}`}>
             <div
               className={`rounded-3xl p-2 mb-2 break-words whitespace-pre-wrap ${
@@ -61,32 +57,29 @@ const Message = ({ message, isUser, isLoading, image }) => {
         </div>
       </div>
 
-      {/* Modal */}
       {modalImage && (
         <div
           className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50"
           onClick={closeModal}
         >
           <div
-            className="relative bg-white p-4 rounded-lg"
-            style={{
-              width: "70%",
-              height: "auto",
-              border: "5px solid white",
-            }}
-            onClick={(e) => e.stopPropagation()} // Modalın içərisinə basıldıqda bağlanmasın
+            className="relative bg-white p-1 rounded-lg"
+            onClick={(e) => e.stopPropagation()}
           >
-            {/* Modalın bağlama düyməsi */}
             <button
               className="absolute top-2 right-2 text-black text-xl font-bold"
               onClick={closeModal}
             >
               &times;
             </button>
-            {/* Şəkil */}
+
             <img
               className="w-full h-auto rounded-lg"
               src={modalImage}
+              style={{
+                maxHeight: "70vh",
+                maxWidth: "70vh",
+              }}
               alt="Modal Image"
             />
           </div>
